467a61f078c5d7e8b3c938bb82309e21e6f5d70e
Turner Thornberry <turner.thornberry@gmail.com>
2019-05-23 20:08:11 -0400

    Storyboard UI skeleton complete. Prep for segue logic

---
+++ Blast Off/PCIController.swift
@@ -0,0 +1,74 @@
+//
+//  PCIController.swift
+//  Blast Off
+//
+//  Created by Turner Thornberry on 5/23/19.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class PCIController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var rxTField: UITextField!
+    @IBOutlet weak var ryTField: UITextField!
+    @IBOutlet weak var rzTField: UITextField!
+    @IBOutlet weak var vxTField: UITextField!
+    @IBOutlet weak var vyTField: UITextField!
+    @IBOutlet weak var vzTField: UITextField!
+
+
+    @IBOutlet weak var rxSlider: UISlider!
+    @IBOutlet weak var rySlider: UISlider!
+    @IBOutlet weak var rzSlider: UISlider!
+    @IBOutlet weak var vxSlider: UISlider!
+    @IBOutlet weak var vySlider: UISlider!
+    @IBOutlet weak var vzSlider: UISlider!
+
+
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func setupTextFields(){
+        
+    }
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    
+    
+    
+
+}
